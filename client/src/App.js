b35ebab28e53520400241b792e4f50688016a336
Muhire-Josue <rutayisire.josue09@gmail.com>
2020-05-04 08:37:55 +0200

    chore(basic layout components)
    - add signup component design
    - add login component design
    - add landing component design

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
-import React, {Fragment} from 'react';
-
+import React, { Fragment } from 'react';
+import NavBar from './components/layout/NavBar';
+import Landing from './components/layout/Landing';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LogIn from './components/auth/LogIn';
+import Register from './components/auth/Register';
+import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
 const App = () => {
   return (
-    <Fragment className="App">
-      <h1>App</h1>
-    </Fragment>
+    <Provider store={store}>
+    <Router>
+      <Fragment className="App">
+        <NavBar />
+        <Route exact path='/' component={Landing} />
+          <section className="container">
+            <Switch>
+          <Route exact path='/login' component={LogIn} />
+          <Route exact path='/register' component={Register} />
+            </Switch>
+        </section>
+      </Fragment>
+    </Router>
+    </Provider>
   );
 }
 
