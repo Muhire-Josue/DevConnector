8d73f0f738817150f879dc9633fdf4a36b19f220
Muhire-Josue <rutayisire.josue09@gmail.com>
2020-05-06 09:10:42 +0200

    feature(alert)
    - add alert component
    - add alert auto-remove

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,11 +1,11 @@
-import { Set_ALERT, REMOVE_ALERT } from '../actions/types';
+import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
 
 const initialState = [];
 
 export default (state = initialState, action) => {
     const { type, payload } = action;
     switch (type) {
-        case Set_ALERT:
+        case SET_ALERT:
             return [...state, payload];
         case REMOVE_ALERT:
             return state.filter(data => data.id !== payload);
